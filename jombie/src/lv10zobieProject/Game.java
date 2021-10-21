f41b63e8261cbb7be0a0fa74201a4ffcef887ec4
KimHeeRull <KimHeeRull@KimHeeRull>
2021-10-21 21:18:29 +0900

    start

---
+++ jombie/src/lv10zobieProject/Game.java
@@ -0,0 +1,15 @@
+package lv10zobieProject;
+
+public class Game {
+
+	private static Game instance = new Game();
+	
+	
+	public static Game getInstance() {
+		return instance;
+	}
+
+	public void run() {
+		System.out.println("====모험시작=====");
+	}
+}
